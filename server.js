819473bee91a3303cf86fa28ca67342e09ec6b03
Prateek Khindri <prateek.khindri90@gmail.com>
2022-05-08 14:53:31 +1000

    Updated API endpoints

--- server.js
+++ server.js
@@ -3,6 +3,31 @@ const app = express();
 
 const PORT = 8000;
 
+// Task API endpoints
+app.get("/api/v1/tasks", (req, res) => {
+  res.json({
+    message: "Hello get response",
+  });
+});
+
+app.post("/api/v1/tasks", (req, res) => {
+  res.json({
+    message: "Hello post response",
+  });
+});
+
+app.patch("/api/v1/tasks", (req, res) => {
+  res.json({
+    message: "Hello patch response",
+  });
+});
+
+app.delete("/api/v1/tasks", (req, res) => {
+  res.json({
+    message: "Hello delete response",
+  });
+});
+
 app.get("/", (req, res) => {
   res.json({
     message: "You have reached the not to do api server",
