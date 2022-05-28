4b35a71d37b2ca3833931fb8b8519580982620fc
Prateek Khindri <prateek.khindri90@gmail.com>
2022-05-28 12:19:33 +1000

    updated

--- not-to-do-list-api/server.js
+++ not-to-do-list-api/server.js
@@ -29,9 +29,9 @@ app.use(
   express.static(path.resolve(__dirname, "./react-not-to-do-list/build"))
 );
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(
-    path.resolve(__dirname, "./react-not-to-do-list/build", "index.html")
+    path.resolve(__dirname, "./react-not-to-do-list/build/index.html")
   );
 });
 
